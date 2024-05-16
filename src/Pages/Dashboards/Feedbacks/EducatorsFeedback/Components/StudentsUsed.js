b8e8129bd5d19492664c21aef4c1374867751fb9
Lama Ghusn <lama.ghusn@cts.ae>
2024-05-16 16:49:44 +0300

    mm

--- src/Pages/Dashboards/Feedbacks/EducatorsFeedback/Components/StudentsUsed.js
+++ src/Pages/Dashboards/Feedbacks/EducatorsFeedback/Components/StudentsUsed.js
@@ -3,54 +3,60 @@ import { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
 
 const StudentsUsed = (props) => {
-    const [usedNumbers, setUsedNumbers] = useState([])
-    const getUsedNumbers = () => {
-        var config = {
-            method: "get",
-            url: process.env.REACT_APP_API_URL + "Dashboard/GetEducatorYesNo/" + props.parameters,
-            headers: {
-                "Access-Control-Allow-Origin": process.env.REACT_APP_Host,
-            },
-        };
-        axios(config).then(function (response) {
-            if (response.status == 200) {
-                setUsedNumbers(response.data.studentsUsed);
-            }
-        });
-    }
-    var series = [{
-        data: usedNumbers
-    }];
-
-    var options = {
-        plotOptions: {
-            bar: {
-                borderRadius: 4,
-                borderRadiusApplication: 'end',
-                horizontal: true,
-            },
-        },
-        dataLabels: {
-            enabled: false
-        },
-        xaxis: {
-            categories: ['Yes', 'No'],
-        },
-        colors: ["#0078E6", "#05BC86"],
+  const [usedNumbers, setUsedNumbers] = useState([]);
+  const getUsedNumbers = () => {
+    var config = {
+      method: "get",
+      url:
+        process.env.REACT_APP_API_URL +
+        "Dashboard/GetEducatorYesNo/" +
+        props.parameters,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.REACT_APP_Host,
+      },
     };
-    useEffect(() => {
-        getUsedNumbers()
-    }, [])
-    return (
-        <>
-            <ReactApexChart
-                type="bar"
-                options={options}
-                series={series}
-                height={280}
-            />
-        </>
-    );
-}
+    axios(config).then(function (response) {
+      if (response.status == 200) {
+        setUsedNumbers(response.data.studentsUsed);
+      }
+    });
+  };
+  var series = [
+    {
+      data: usedNumbers,
+    },
+  ];
+
+  var options = {
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        borderRadiusApplication: "end",
+        horizontal: true,
+        distributed: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: ["Yes", "No"],
+    },
+    colors: ["#0078E6", "#05BC86"],
+  };
+  useEffect(() => {
+    getUsedNumbers();
+  }, []);
+  return (
+    <>
+      <ReactApexChart
+        type="bar"
+        options={options}
+        series={series}
+        height={280}
+      />
+    </>
+  );
+};
 
 export default StudentsUsed;
